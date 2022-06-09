d9c88691ec201e3821a89bf21e482a5e0f36cea1
christoff-linde <christoff.linde@gmail.com>
2022-06-09 13:04:35 +0200

    feature: add amplify config
    Added Predictions, hosting, and S3 Storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    awsworkshopwebf9a1aabe: {
+      IdentityPoolId: "string"
+      IdentityPoolName: "string"
+      UserPoolId: "string"
+      UserPoolArn: "string"
+      UserPoolName: "string"
+      AppClientIDWeb: "string"
+      AppClientID: "string"
+    }
+  }
+  predictions: {
+    identifyEntities1f7625d5: {
+      region: "string"
+      celebrityDetectionEnabled: "string"
+      maxEntities: "string"
+      collectionId: "string"
+    }
+  }
+  storage: {
+    s3806ffe8b: {
+      BucketName: "string"
+      Region: "string"
+    }
+  }
+  function: {
+    RekognitionIndexFacesTrigger5c7cff5b: {
+      Name: "string"
+      Arn: "string"
+      Region: "string"
+      LambdaExecutionRole: "string"
+    }
+  }
+}
