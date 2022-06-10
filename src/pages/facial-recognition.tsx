8e47573ffe31f79154dae683f0d5db0d04eb5118
christoff-linde <christoff.linde@gmail.com>
2022-06-10 16:27:12 +0200

    feature: update layout & add FileUploadForm to page

--- src/pages/facial-recognition.tsx
+++ src/pages/facial-recognition.tsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { FileUploadForm } from "../components/FileUpload"
 
 const FacialRecognitionPage = () => {
   return (
     <>
-      <h1 className="font-sans font-extrabold text-5xl">Facial Recognition</h1>
+      <h1 className="font-sans font-extrabold text-3xl lg:text-5xl">
+        Facial Recognition
+      </h1>
+      <FileUploadForm />
     </>
   )
 }
